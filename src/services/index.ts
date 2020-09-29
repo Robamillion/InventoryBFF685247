6114561145596b2ea69634cc4955a4af598ffe84
Robin Pannu <robinpannu@Robins-MBP.home>
2020-09-29 14:11:10 -0500

    Adds service implementation

--- src/services/index.ts
+++ src/services/index.ts
@@ -8,8 +8,10 @@ export * from './project.api';
 export * from './project.service';
 export * from './task.api';
 export * from './task.service';
+export * from './stock-items.api';
+
 
 Container.bind(TaskApi).to(TaskService).scope(Scope.Singleton);
 Container.bind(ProjectApi).to(ProjectService).scope(Scope.Singleton);
-export * from './stock-items.api';
-export * from './stock-items-mock.service';
+
+
